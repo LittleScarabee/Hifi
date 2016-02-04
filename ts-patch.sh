c1b6df34a954754def374171e23bacec9f9669a8
Fred <little.scarabee@outlook.fr>
2016-02-04 20:58:36 +0100

    Update ts-patch.sh
    Add variables for all path and filename

--- ts-patch.sh
+++ ts-patch.sh
@@ -5,41 +5,52 @@ echo "#    Patch Tiny     #"
 echo "#                   #"
 echo "#####################"
 echo ""
+folderPlayhrt="/home/tc/sq/"
+folderBootLocal="/opt/"
+folderSubmitAudio="/opt/www/cgi-bin/"
+folderIndex="/opt/www/"
+
+filePlayhrt="pipe_playhrt_custom"
+fileBootLocal="bootlocal.sh"
+fileSubmitAudio="submit_audio.yy"
+fileIndex="index.yy"
+
+urlGitHub="https://github.com/LittleScarabee/Hifi/blob/master/"
+
 cd /tmp
 
 echo " >> Download all files..."
 echo ""
-sudo /usr/local/bin/wget https://github.com/LittleScarabee/Hifi/blob/master/bootlocal.sh
-sudo /usr/local/bin/wget https://github.com/LittleScarabee/Hifi/blob/master/index.yy
-sudo /usr/local/bin/wget https://github.com/LittleScarabee/Hifi/blob/master/pipe_playhrt_custom
-sudo /usr/local/bin/wget https://github.com/LittleScarabee/Hifi/blob/master/submit_audio.yy
+sudo /usr/local/bin/wget $urlGitHub$fileBootLocal
+sudo /usr/local/bin/wget $urlGitHub$fileIndex
+sudo /usr/local/bin/wget $urlGitHub$filePlayhrt
+sudo /usr/local/bin/wget $urlGitHub$fileSubmitAudio
 echo " >> Download completed..."
 echo ""
 
 echo " >> Backup files..."
 echo ""
-sudo /bin/cp /home/tc/sq/pipe_playhrt /home/tc/sq/bkp.pipe_playhrt
-sudo /bin/cp /opt/bootlocal.sh /opt/bkp.bootlocal.sh
-sudo /bin/cp /opt/www/cgi-bin/submit_audio.yy /opt/www/cgi-bin/bkp.submit_audio.yy
-sudo /bin/cp /opt/www/index.yy /opt/www/bkp.index.yy
+sudo /bin/cp $folderBootLocal$fileBootLocal $folderBootLocal$fileBootLocal.bkp
+sudo /bin/cp $folderSubmitAudio$fileSubmitAudio $folderSubmitAudio$fileSubmitAudio.bkp
+sudo /bin/cp $folderIndex$fileIndex $folderIndex$fileIndex.bkp
 
 echo " >> Replace all files..."
 echo ""
-sudo /bin/cp /tmp/pipe_playhrt_custom /home/tc/sq/pipe_playhrt_custom
-sudo /bin/cp /tmp/bootlocal.sh /opt/bootlocal.sh
-sudo /bin/cp /tmp/submit_audio.yy /opt/www/cgi-bin/submit_audio.yy
-sudo /bin/cp /tmp/index.yy /opt/www/index.yy
+sudo /bin/cp /tmp/$filePlayhrt $folderPlayhrt$filePlayhrt
+sudo /bin/cp /tmp/$fileBootLocal $folderBootLocal$fileBootLocal
+sudo /bin/cp /tmp/$fileSubmitAudio $folderSubmitAudio$fileSubmitAudio
+sudo /bin/cp /tmp/$fileIndex $folderIndex$fileIndex
 
 echo " >> Apply right permissions for each files..."
 echo ""
-sudo /bin/chmod 755 /opt/bootlocal.sh
-sudo /bin/chown root:staff /opt/bootlocal.sh
+sudo /bin/chmod 755 $folderBootLocal$fileBootLocal
+sudo /bin/chown root:staff $folderBootLocal$fileBootLocal
 echo ""
-sudo /bin/chmod 644 /home/tc/sq/pipe_playhrt_custom
-sudo /bin/chown root:staff /home/tc/sq/pipe_playhrt_custom
+sudo /bin/chmod 644 $folderPlayhrt$filePlayhrt
+sudo /bin/chown root:staff $folderPlayhrt$filePlayhrt
 echo ""
-sudo /bin/chmod 755 /opt/www/cgi-bin/submit_audio.yy
-sudo /bin/chown root:root /opt/www/cgi-bin/submit_audio.yy
+sudo /bin/chmod 755 $folderSubmitAudio$fileSubmitAudio
+sudo /bin/chown root:root $folderSubmitAudio$fileSubmitAudio
 echo ""
-sudo /bin/chmod 755 /opt/www/index.yy
-sudo /bin/chown root:root /opt/www/index.yy
+sudo /bin/chmod 755 $folderIndex$fileIndex
+sudo /bin/chown root:root $folderIndex$fileIndex
