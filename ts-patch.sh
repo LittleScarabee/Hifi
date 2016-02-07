40781460ad9889808d68968fcb5a71071c49c76d
Fred <little.scarabee@outlook.fr>
2016-02-07 10:39:58 +0100

    Update ts-patch.sh
    Patch printf

--- ts-patch.sh
+++ ts-patch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-printf "#####################"
-printf "#  Patch Tiny 1.6   #"
-printf "#####################"
-printf ""
+printf "#####################\n"
+printf "#  Patch Tiny 1.6   #\n"
+printf "#####################\n"
+printf "\n"
 folderPlayhrt="/home/tc/sq/"
 folderBootLocal="/opt/"
 folderSubmitAudio="/opt/www/cgi-bin/"
@@ -18,25 +18,25 @@ fileCustomConvert="/home/custom-convert.conf"
 urlGitHub="https://raw.githubusercontent.com/LittleScarabee/Hifi/master/"
 
 cd /tmp
-printf ">>> START Update..."
+printf ">>> START Update...\n"
 printf " >> Download all files..."
 sudo /usr/local/bin/wget $urlGitHub$fileBootLocal
 sudo /usr/local/bin/wget $urlGitHub$fileIndex
 sudo /usr/local/bin/wget $urlGitHub$filePlayhrt
 sudo /usr/local/bin/wget $urlGitHub$fileSubmitAudio
-printf " >> Download completed..."
+printf " >> Download completed...\n"
 
 printf " >> Backup files..."
 sudo /bin/cp $folderBootLocal$fileBootLocal $folderBootLocal$fileBootLocal.bkp
 sudo /bin/cp $folderSubmitAudio$fileSubmitAudio $folderSubmitAudio$fileSubmitAudio.bkp
 sudo /bin/cp $folderIndex$fileIndex $folderIndex$fileIndex.bkp
-printf " >> Done !"
+printf " Done !\n"
 printf " >> Replace all files..."
 sudo /bin/cp /tmp/$filePlayhrt $folderPlayhrt$filePlayhrt
 sudo /bin/cp /tmp/$fileBootLocal $folderBootLocal$fileBootLocal
 sudo /bin/cp /tmp/$fileSubmitAudio $folderSubmitAudio$fileSubmitAudio
 sudo /bin/cp /tmp/$fileIndex $folderIndex$fileIndex
-printf " >> Done !"
+printf " Done !\n"
 printf " >> Apply right permissions for each files..."
 sudo /bin/chmod 755 $folderBootLocal$fileBootLocal
 sudo /bin/chown root:staff $folderBootLocal$fileBootLocal
@@ -46,12 +46,12 @@ sudo /bin/chmod 755 $folderSubmitAudio$fileSubmitAudio
 sudo /bin/chown root:root $folderSubmitAudio$fileSubmitAudio
 sudo /bin/chmod 755 $folderIndex$fileIndex
 sudo /bin/chown root:root $folderIndex$fileIndex
-printf " >> Done !"
+printf " Done !\n"
 printf " >> Add AIF Compatibily..."
 printf "##" >>$fileCustomConvert
 printf "## Aif Compatibility" >>$fileCustomConvert
 printf "##" >>$fileCustomConvert
 printf "aif pcm * *" >>$fileCustomConvert
 printf "        -" >>$fileCustomConvert
-printf " >> Done !"
-printf ">>> END Update..."
+printf " Done !\n"
+printf ">>> END Update...\n"
