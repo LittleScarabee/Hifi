0ab3b71de3b72e09ade9599619001eb142525d1e
Fred <little.scarabee@outlook.fr>
2016-02-07 10:37:05 +0100

    Update ts-patch.sh
    Replace echo by printf

--- ts-patch.sh
+++ ts-patch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-echo "#####################"
-echo "#  Patch Tiny 1.6   #"
-echo "#####################"
-echo ""
+printf "#####################"
+printf "#  Patch Tiny 1.6   #"
+printf "#####################"
+printf ""
 folderPlayhrt="/home/tc/sq/"
 folderBootLocal="/opt/"
 folderSubmitAudio="/opt/www/cgi-bin/"
@@ -18,26 +18,26 @@ fileCustomConvert="/home/custom-convert.conf"
 urlGitHub="https://raw.githubusercontent.com/LittleScarabee/Hifi/master/"
 
 cd /tmp
-echo ">>> START Update..."
-echo " >> Download all files..."
+printf ">>> START Update..."
+printf " >> Download all files..."
 sudo /usr/local/bin/wget $urlGitHub$fileBootLocal
 sudo /usr/local/bin/wget $urlGitHub$fileIndex
 sudo /usr/local/bin/wget $urlGitHub$filePlayhrt
 sudo /usr/local/bin/wget $urlGitHub$fileSubmitAudio
-echo " >> Download completed..."
+printf " >> Download completed..."
 
-echo " >> Backup files..."
+printf " >> Backup files..."
 sudo /bin/cp $folderBootLocal$fileBootLocal $folderBootLocal$fileBootLocal.bkp
 sudo /bin/cp $folderSubmitAudio$fileSubmitAudio $folderSubmitAudio$fileSubmitAudio.bkp
 sudo /bin/cp $folderIndex$fileIndex $folderIndex$fileIndex.bkp
-echo -e "Done !"
-echo " >> Replace all files..."
+printf " >> Done !"
+printf " >> Replace all files..."
 sudo /bin/cp /tmp/$filePlayhrt $folderPlayhrt$filePlayhrt
 sudo /bin/cp /tmp/$fileBootLocal $folderBootLocal$fileBootLocal
 sudo /bin/cp /tmp/$fileSubmitAudio $folderSubmitAudio$fileSubmitAudio
 sudo /bin/cp /tmp/$fileIndex $folderIndex$fileIndex
-echo -e "Done !"
-echo " >> Apply right permissions for each files..."
+printf " >> Done !"
+printf " >> Apply right permissions for each files..."
 sudo /bin/chmod 755 $folderBootLocal$fileBootLocal
 sudo /bin/chown root:staff $folderBootLocal$fileBootLocal
 sudo /bin/chmod 644 $folderPlayhrt$filePlayhrt
@@ -46,12 +46,12 @@ sudo /bin/chmod 755 $folderSubmitAudio$fileSubmitAudio
 sudo /bin/chown root:root $folderSubmitAudio$fileSubmitAudio
 sudo /bin/chmod 755 $folderIndex$fileIndex
 sudo /bin/chown root:root $folderIndex$fileIndex
-echo -e "Done !"
-echo " >> Add AIF Compatibily..."
-echo "##" >>$fileCustomConvert
-echo "## Aif Compatibility" >>$fileCustomConvert
-echo "##" >>$fileCustomConvert
-echo "aif pcm * *" >>$fileCustomConvert
-echo "        -" >>$fileCustomConvert
-echo -e "Done !"
-echo ">>> END Update..."
+printf " >> Done !"
+printf " >> Add AIF Compatibily..."
+printf "##" >>$fileCustomConvert
+printf "## Aif Compatibility" >>$fileCustomConvert
+printf "##" >>$fileCustomConvert
+printf "aif pcm * *" >>$fileCustomConvert
+printf "        -" >>$fileCustomConvert
+printf " >> Done !"
+printf ">>> END Update..."
